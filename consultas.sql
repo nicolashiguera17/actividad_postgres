f6534bb45ab91afac09614fe5b762c561aa11fd7
nicolashiguera17 <189983438+nicolashiguera17@users.noreply.github.com>
2025-08-22 16:27:24 -0500

    feat: :sparkles: segunda consulta

--- consultas.sql
+++ consultas.sql
@@ -8,3 +8,15 @@ JOIN productos p ON cp.id_producto = p.id_producto
 GROUP BY p.id_producto, p.nombre
 ORDER BY unidades_vendidas DESC
 LIMIT 10;
+
+
+-- 2 Categorías con mayor número de productos vendidos --
+
+SELECT c.id_categoria, 
+       c.descripcion,
+       SUM(cp.cantidad) AS total_vendidos
+FROM compras_productos cp
+JOIN productos p ON cp.id_producto = p.id_producto
+JOIN categorias c ON p.id_categoria = c.id_categoria
+GROUP BY c.id_categoria, c.descripcion
+ORDER BY total_vendidos DESC;
